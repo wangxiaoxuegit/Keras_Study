97c6e03e83bf55e5211ac04a2cf2b9907501eed2
wangxiaoxuegit <51707555+wangxiaoxuegit@users.noreply.github.com>
2019-11-17 18:30:25 +0800

    Create cnn_mnist_another_build_method.py

---
+++ 3_cnn/cnn_mnist_another_build_method.py
@@ -0,0 +1,53 @@
+
+from keras.models import Model
+from keras.layers import Dense, Conv2D, MaxPooling2D, Flatten, Dropout, Input
+from keras.datasets import mnist
+from keras.utils import to_categorical
+from keras import backend as K
+
+# data pre-process
+(X_train, Y_train), (X_test, Y_test) = mnist.load_data()
+if K.image_data_format() == 'channels_first':
+    X_train = X_train.reshape(X_train.shape[0], 1, 28, 28)/255
+    X_test = X_test.reshape(X_test.shape[0], 1, 28, 28)/255
+    input_shape = (1, 28, 28)
+else:
+    X_train = X_train.reshape(X_train.shape[0], 28, 28, 1)/255
+    X_test = X_test.reshape(X_test.shape[0], 28, 28, 1)/255
+    input_shape = (28, 28, 1)
+Y_train = to_categorical(Y_train, num_classes=10)
+Y_test = to_categorical(Y_test, num_classes=10)
+
+# build model
+# model = Sequential()
+# model.add(Conv2D(32, kernel_size=(5, 5), activation='relu', input_shape=input_shape))
+# model.add(MaxPooling2D(pool_size=(2, 2)))
+# model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+# model.add(MaxPooling2D(pool_size=(2, 2)))
+# model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+# model.add(MaxPooling2D(pool_size=(2, 2)))
+# model.add(Dropout(0.25))
+# model.add(Flatten())
+# model.add(Dense(1024, activation='relu'))
+# model.add(Dropout(0.5))
+# model.add(Dense(10, activation='softmax'))
+# model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# build model in another method
+inputs = Input(shape=input_shape)
+x = Conv2D(32, kernel_size=(5, 5), activation='relu')(inputs)
+x = MaxPooling2D(pool_size=(2, 2))(x)
+x = Conv2D(64, kernel_size=(3, 3), activation='relu')(x)
+x = MaxPooling2D(pool_size=(2, 2))(x)
+x = Conv2D(128, kernel_size=(3, 3), activation='relu')(x)
+x = MaxPooling2D(pool_size=(2, 2))(x)
+x = Dropout(0.25)(x)
+x = Flatten()(x)
+x = Dense(1024, activation='relu')(x)
+x = Dropout(0.5)(x)
+y = Dense(10, activation='softmax')(x)
+model = Model(inputs=inputs, outputs=y)
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# train
+model.fit(X_train, Y_train, epochs=1, batch_size=32, validation_data=(X_test, Y_test))
