5c4d84aec12b6349dfa959cf0543cab9c0be44be
wangxiaoxuegit <51707555+wangxiaoxuegit@users.noreply.github.com>
2019-11-17 16:37:34 +0800

    Create cnn_mnist.py

---
+++ 3_cnn/cnn_mnist.py
@@ -0,0 +1,39 @@
+
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D, MaxPooling2D, Flatten, Dropout
+from keras.datasets import mnist
+from keras.utils import to_categorical
+from keras import backend as K
+
+# data pre-process
+(X_train, Y_train), (X_test, Y_test) = mnist.load_data()
+if K.image_data_format() == 'channels_first':
+    X_train = X_train.reshape(X_train.shape[0], 1, 28, 28)/255
+    X_test = X_test.reshape(X_test.shape[0], 1, 28, 28)/255
+    data_train_x = data_train_x.reshape(data_train_x.shape[0], 1, img_rows, img_cols)
+    data_test = data_test.reshape(data_test.shape[0], 1, img_rows, img_cols)
+    input_shape = (1, 28, 28)
+else:
+    X_train = X_train.reshape(X_train.shape[0], 28, 28, 1)/255
+    X_test = X_test.reshape(X_test.shape[0], 28, 28, 1)/255
+    input_shape = (28, 28, 1)
+Y_train = to_categorical(Y_train, num_classes=10)
+Y_test = to_categorical(Y_test, num_classes=10)
+
+# build model
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(5, 5), activation='relu', input_shape=input_shape))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(1024, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(10, activation='softmax'))
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# train
+model.fit(X_train, Y_train, epochs=10, batch_size=32, validation_data=(X_test, Y_test))
