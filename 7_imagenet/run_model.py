4f471ca0495ea97acb988ad49a266703587b3da7
wangxiaoxuegit <51707555+wangxiaoxuegit@users.noreply.github.com>
2019-11-19 14:43:13 +0800

    Create run_model.py

---
+++ 7_imagenet/run_model.py
@@ -0,0 +1,14 @@
+
+from cnnnet import AlexNet
+from cnnnet import VGG_16
+from cnnnet import VGG_19
+
+
+alexnet = AlexNet()
+alexnet.summary()
+
+vgg16 = VGG_16()
+vgg16.summary()
+
+vgg19 = VGG_19()
+vgg19.summary()
